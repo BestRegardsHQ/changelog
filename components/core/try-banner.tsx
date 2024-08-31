4d3c30d354a0cc3c3a453ff95827427b83a0cf40
ohansemmanuel <ohansemmanuel@outlook.com>
2024-08-31 10:55:25 +0200

    ✨ feat: tweaks banner footer

--- components/core/try-banner.tsx
+++ components/core/try-banner.tsx
@@ -7,6 +7,7 @@ import {
   Container,
   em,
   getBreakpointValue,
+  Group,
   Image,
   Space,
   Stack,
@@ -24,7 +25,9 @@ function TryJuneBanner() {
 
   return (
     <Container size="xl">
-      <Space h={isMobileViewport ? "10vh" : "10vh"} />
+      <Space h={isMobileViewport ? "10vh" : "20vh"} />
+
+      <Space h="xl" />
 
       <Image
         pb="5vh"
@@ -35,9 +38,50 @@ function TryJuneBanner() {
         }
       />
 
+      <Group
+        sx={{
+          justifyContent: "center",
+        }}
+        mb="xl"
+      >
+        <a
+          href="https://www.producthunt.com/posts/bestregards?embed=true&utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_souce=badge-bestregards"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=454223&theme=dark&period=weekly&topic_id=164"
+            alt="BestRegards - Never&#0032;pay&#0032;another&#0032;email&#0032;marketing&#0032;subscription | Product Hunt"
+            style={{
+              width: "250px",
+              height: "54px",
+            }}
+            width="250"
+            height="54"
+          />
+        </a>
+
+        <a
+          href="https://www.producthunt.com/posts/bestregards?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-bestregards"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=454223&theme=dark&period=daily"
+            alt="BestRegards - Never&#0032;pay&#0032;another&#0032;email&#0032;marketing&#0032;subscription | Product Hunt"
+            style={{
+              width: "250px",
+              height: "54px",
+            }}
+            width="250"
+            height="54"
+          />
+        </a>
+      </Group>
+
       <Center mb="xl">
         <Box>
-          <Button size="xl" component="a" href="https://www.bestregards.me/signup" compact>
+          <Button size="xl" component="a" href="https://www.bestregards.me/signup">
             Create your free account
           </Button>
           <Space h="xl" />
